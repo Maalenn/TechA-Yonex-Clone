8e50aba4a5e48835aab8a190d864a68532faf030
SreangLyhour12 <slyhour69@gmail.com>
2024-04-29 10:10:04 +0700

    feat(homePage): initial index.js
    - create objects for each sport card component

---
+++ 2nd-mission/assets/js/index.js
@@ -0,0 +1,171 @@
+// Sport card object
+const arrObj = [
+    {
+        img: "https://www.yonex.com/media/catalog/category/NANOFLARE_800.png",
+        title: "BADMINTON"
+    },
+    {
+        img: "https://www.yonex.com/media/catalog/category/ezone-100.png",
+        title: "TENNIS"
+    },
+    {
+        img: "https://www.yonex.com/media/catalog/category/EZONE_GT_d_ts_Head.png",
+        title: "GOLF"
+    },
+    {
+        img: "https://www.yonex.com/media/catalog/category/Carbon-Cruise-Aerus.png",
+        title: "RUNNING"
+        
+    },
+    
+]
+// Badminton object
+const badmintonObj = [
+    {
+        img: "../assets/icons/image/racquets_240306.png",
+        title: "RAQUEST"
+        
+    },
+    {
+        img: "../assets/icons/image/strings_240306.webp",
+        title: "STRINGS"
+        
+    },
+    {
+        img: "../assets/icons/image/shuttlecocks_img.webp",
+        title: "SHUTTLECOCKS"
+        
+    },
+    {
+        img: "../assets/icons/image/10566_011_520x320.webp",
+        title: "APPAREL"
+        
+    },
+    {
+        img: "../assets/icons/image/shoes_240306.webp",
+        title: "SHOES"
+        
+    },
+    {
+        img: "../assets/icons/image/bag.webp",
+        title: "BAGS"
+        
+    },
+    {
+        img: "../assets/icons/image/accessories_img2.webp",
+        title: "ACCESSORIES"
+        
+    },
+    {
+        img: "../assets/icons/image/athletes_240306.webp",
+        title: "ATHLETES"
+        
+    },
+]
+// golfobj
+const golfObj = [
+    {
+        img: "../assets/icons/image/thm_club_ezone.webp",
+        title: "RAQUEST"
+        
+    },
+    {
+        img: "../assets/icons/image/Mega-Menu-Golf-Shaft_1.webp",
+        title: "SHAFTS"
+        
+    },
+    {
+        img: "../assets/icons/image/Mega-Golf-Bag.webp",
+        title: "GEAR"
+        
+    },
+    {
+        img: "../assets/icons/image/golf-athletes.webp",
+        title: "APPAREL"
+        
+    }
+]
+// tennisobj
+const tennisObj = [
+    {
+        img: "../assets/icons/image/raquet-tennis.webp",
+        title: "RAQUEST"
+        
+    },
+    {
+        img: "../assets/icons/image/strings-tennis.webp",
+        title: "SHAFTS"
+        
+    },
+    {
+        img: "../assets/icons/image/tennis-ball.webp",
+        title: "GEAR"
+        
+    },
+    {
+        img: "../assets/icons/image/apparel-tennis.webp",
+        title: "APPAREL"
+        
+    },
+    {
+        img: "../assets/icons/image/shoes-tennis.webp",
+        title: "SHOES"
+        
+    },
+    {
+        img: "../assets/icons/image/tennis_Bag_.webp",
+        title: "BAGS"
+        
+    },
+    {
+        img: "../assets/icons/image/accessories-tennis.webp",
+        title: "ACCESSORIES"
+        
+    },
+    {
+        img: "../assets/icons/image/tennis-athlete.webp",
+        title: "ATHLETES"
+        
+    },
+]
+// runningobj
+const runningObj = [
+    {
+        img: "../assets/icons/image/shoe-orange.webp",
+        title: "MEN"
+        
+    },
+    {
+        img: "../assets/icons/image/shoe-white.webp",
+        title: "WOMEN"
+        
+    },
+    {
+        img: "../assets/icons/image/shoe-brown.webp",
+        title: "UNISEX"
+        
+    },
+]
+// snowboardobj
+const snowboardObj = [
+    {
+        img: "../assets/icons/image/skate.webp",
+        title: "BOARD"
+        
+    },
+    {
+        img: "../assets/icons/image/skate-shoe.webp",
+        title: "BOOT & BINDING"
+        
+    },
+    {
+        img: "../assets/icons/image/skate-coat.webp",
+        title: "GEAR"
+        
+    },
+    {
+        img: "../assets/icons/image/apparel-tennis.webp",
+        title: "APPAREL"
+        
+    },
+]
