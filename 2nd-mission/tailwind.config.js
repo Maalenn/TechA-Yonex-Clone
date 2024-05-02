0b3241d74538a9ba8c1dccf82a6d0ee73a7178b5
Prak Sreypov <sreypovprak88@gmail.com>
2024-05-02 20:08:49 +0700

    feat(runningPage): change direction of right navigation bar

--- 2nd-mission/tailwind.config.js
+++ 2nd-mission/tailwind.config.js
@@ -3,32 +3,30 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
+      colors: {
         primary: {
-          100: '#006cb7',
-          200: '#1c5dad',
-          300: '#0a56c2'
+          100: "#006cb7",
+          200: "#1c5dad",
+          300: "#0a56c2",
         },
         success: {
-          'light': '#e5efe5',
-          'dark': '#006400',
+          light: "#e5efe5",
+          dark: "#006400",
         },
         secondary: {
-          100: '#f5f5f5',
-          200: '#6e7677',
-          300: '#a8a8a8',
-          400: '#595959',
-          500: '#53595a',  
-          600: '#1f2427',
-          700: '#24292b',
-               
-        },        
-        fontFamily: {
-          'Oswald': ['Oswald', 'Lato', 'sans-serif']
-        }
-      }
+          100: "#f5f5f5",
+          200: "#6e7677",
+          300: "#a8a8a8",
+          400: "#595959",
+          500: "#53595a",
+          600: "#1f2427",
+          700: "#24292b",
+        },
+      },
+      fontFamily: {
+        Oswald: ["Oswald", "Lato", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
