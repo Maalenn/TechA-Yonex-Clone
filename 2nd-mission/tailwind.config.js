4ead9ec06a2946da8513b583ec60ad732f85bb61
Tep chounan <tch23125@gmail.com>
2024-05-03 11:29:50 +0700

    resolve conflict and add new font

--- 2nd-mission/tailwind.config.js
+++ 2nd-mission/tailwind.config.js
@@ -3,41 +3,42 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        primary: {
-          100: "#006cb7",
-          200: "#1c5dad",
-          300: "#0a56c2",
+        colors: {
+          primary: {
+            100: "#006cb7",
+            200: "#1c5dad",
+            300: "#0a56c2",
+          },     
+          success: {
+            light: "#e5efe5",
+            dark: "#006400",
+          },
+          secondary: {
+            100: "#f5f5f5",
+            200: "#6e7677",
+            300: "#a8a8a8",
+            400: "#595959",
+            500: "#53595a",
+            600: "#1f2427",
+            700: "#24292b",
+          },
         },
-        success: {
-          light: "#e5efe5",
-          dark: "#006400",
+        fontFamily: {
+          Oswald: ["Oswald", "Lato", "sans-serif"],
+          'Figtree' : ['Figtree', 'sans-serif']
         },
-        secondary: {
-          100: "#f5f5f5",
-          200: "#6e7677",
-          300: "#a8a8a8",
-          400: "#595959",
-          500: "#53595a",
-          600: "#1f2427",
-          700: "#24292b",
-        },
-      },
-      fontFamily: {
-        Oswald: ["Oswald", "Lato", "sans-serif"],
-      },
-      screens: {
-        'ssm': "0px",
-         // => @media (min-width: 0px) { ... },
-        'smd': "600px",
-        // => @media (min-width: 600px) { ... },
-        'slg': "900px",
-        // => @media (min-width: 900px) { ... },
-        'sxl': "1200px",
-        // => @media (min-width: 1200px) { ... },
-        'xxl' : "1290px",
-      }
+        screens: {
+          'ssm': "0px",
+          // => @media (min-width: 0px) { ... },
+          'smd': "600px",
+          // => @media (min-width: 600px) { ... },
+          'slg': "900px",
+          // => @media (min-width: 900px) { ... },
+          'sxl': "1200px",
+          // => @media (min-width: 1200px) { ... },
+          'xxl' : "1290px",
+        }  
+      },       
     },
-  },
   plugins: [],
 };
