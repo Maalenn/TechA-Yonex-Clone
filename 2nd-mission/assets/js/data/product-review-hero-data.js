26a279fc63cad7fd603de75f2061d4eba4b13737
Khun Malen <malen8622@gmail.com>
2024-05-21 16:24:45 +0700

    refactor(productReview): change hero cover into dynamic component

---
+++ 2nd-mission/assets/js/data/product-review-hero-data.js
@@ -0,0 +1,9 @@
+export const heroBedminton = [
+  {
+    imgLg: "../assets/images/product-review/heroCover.webp",
+    imgMd: "../assets/images/product-review/heroCover-breakPoint.webp",
+    productName: "bedminton",
+    productType: "racquets",
+    productDes: "bedminton racquets"
+  }
+]
