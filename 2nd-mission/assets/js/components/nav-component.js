bb7970e28894e45e7dba76aeb0572979b0b55739
SreangLyhour12 <slyhour69@gmail.com>
2024-05-23 18:23:09 +0700

    refactor(homePage): add files for navbar component

---
+++ 2nd-mission/assets/js/components/nav-component.js
@@ -0,0 +1,35 @@
+import { badmintonObj, golfObj, snowboardObj, runningObj, aboutObj, tennisObj } from "../data/nav-data.js";
+
+const dataSources = {
+    'badminton-obj': badmintonObj,
+    'tennis-obj': tennisObj,
+    'golf-obj': golfObj,
+    'running-obj': runningObj,
+    'snowboarding-obj': snowboardObj,
+    'about-obj': aboutObj
+};
+
+const CreateCard = (cards) => {
+    return `
+    <div class="grid grid-cols-4 grid-rows-2 gap-4 max-w-[1600px] items-center w-[900px] flex-1">
+        ${cards.map((card) => {
+            return `
+            <div class="flex flex-col items-center justify-center">
+                <a id="hoverLink" href="${card.link}"><img src="${card.img}" alt=""></a>
+                <p class="font-semibold text-[15px] text-black">${card.title}</p>
+            </div>
+            `;
+        }).join("")}
+     </div>
+    `;
+};
+
+class SlideShow extends HTMLElement {
+    connectedCallback() {
+        const dataSource = this.getAttribute('data-source');
+        const data = dataSources[dataSource] || [];
+        this.innerHTML = CreateCard(data);
+    }
+}
+
+customElements.define('card-slide', SlideShow);
---
+++ 2nd-mission/assets/js/data/nav-data.js
@@ -0,0 +1,179 @@
+
+// Badminton object
+export const badmintonObj = [
+    {
+        img: "../assets/images/badminton-images/racquets_240306.png",
+        title: "RAQUEST",
+        link: "../public/product-review.html"
+        
+    },
+    {
+        img: "../assets/images/badminton-images/strings_240306.webp",
+        title: "STRINGS",
+        link: ""
+    },
+    {
+        img: "../assets/images/badminton-images/shuttlecocks_img.webp",
+        title: "SHUTTLECOCKS",
+        link: ""
+    },
+    {
+        img: "../assets/images/badminton-images/10566_011_520x320.webp",
+        title: "APPAREL",
+        link: ""
+    },
+    {
+        img: "../assets/images/badminton-images/shoes_240306.webp",
+        title: "SHOES",
+        link: ""
+    },
+    {
+        img: "../assets/images/badminton-images/bag.webp",
+        title: "BAGS",
+        link: ""
+    },
+    {
+        img: "../assets/images/badminton-images/accessories_img2.webp",
+        title: "ACCESSORIES",
+        link: ""
+    },
+    {
+        img: "../assets/images/badminton-images/athletes_240306.webp",
+        title: "ATHLETES",
+        link: "../public/athletes.html"
+    },
+]
+// golfobj
+export const golfObj = [
+    {
+        img: "../assets/images/golf-images/thm_club_ezone.webp",
+        title: "CLUBS",
+        link: "golf.html"
+    },
+    {
+        img: "../assets/images/golf-images/Mega-Menu-Golf-Shaft_1.webp",
+        title: "SHAFTS",
+        link: ""
+    },
+    {
+        img: "../assets/images/golf-images/Mega-Golf-Bag.webp",
+        title: "GEAR",
+        link: ""
+    },
+    {
+        img: "../assets/images/golf-images/golf-athletes.webp",
+        title: "ATHLETES",
+        link: ""
+    }
+]
+// tennisobj
+export const tennisObj = [
+    {
+        img: "../assets/images/tennis-images/raquet-tennis.webp",
+        title: "RAQUEST",
+        link: "../public/tennis.html"
+    },
+    {
+        img: "../assets/images/tennis-images/strings-tennis.webp",
+        title: "SHAFTS",
+        link: ""
+    },
+    {
+        img: "../assets/images/tennis-images/tennis-ball.webp",
+        title: "BALLS",
+        link: ""
+    },
+    {
+        img: "../assets/images/tennis-images/apparel-tennis.webp",
+        title: "APPAREL",
+        link: ""
+    },
+    {
+        img: "../assets/images/tennis-images/shoes-tennis.webp",
+        title: "SHOES",
+        link: ""
+    },
+    {
+        img: "../assets/images/tennis-images/tennis_Bag_.webp",
+        title: "BAGS",
+        link: ""
+    },
+    {
+        img: "../assets/images/tennis-images/accessories-tennis.webp",
+        title: "ACCESSORIES",
+        link: ""
+    },
+    {
+        img: "../assets/images/tennis-images/tennis-athlete.webp",
+        title: "ATHLETES",
+        link: ""
+    },
+]
+// runningobj
+export const runningObj = [
+    {
+        img: "../assets/images/running-images/shoe-orange.webp",
+        title: "MEN",
+        link: "../public/product-review-runningPage.html"
+    },
+    {
+        img: "../assets/images/running-images/shoe-white.webp",
+        title: "WOMEN",
+        link: ""
+    },
+    {
+        img: "../assets/images/running-images/shoe-brown.webp",
+        title: "UNISEX",
+        link: ""
+    },
+]
+// snowboardobj
+export const snowboardObj = [
+    {
+        img: "../assets/images/snowboard-images/skate.webp",
+        title: "BOARD",
+        link: "snowborading.html"
+    },
+    {
+        img: "../assets/images/snowboard-images/skate-shoe.webp",
+        title: "BOOT & BINDING",
+        link: ""
+    },
+    {
+        img: "../assets/images/snowboard-images/skate-coat.webp",
+        title: "APPAREL",
+        link: ""
+    },
+    {
+        img: "../assets/images/snowboard-images/skate-glove.webp",
+        title: "ACCESSORIES",
+        link: ""
+    },
+]
+// about obj
+export const aboutObj=[
+    {
+        title: "ABOUT US",
+        link: "../public/about-us.html",
+    },
+    {
+        title: "YONEX GROUP",
+        link: "../public/yonex-group.html",
+    },
+    {
+        title: "YONEX DISTRIBUTOR",
+        link: "../public/yonex-distributors.html",
+    },
+    {
+        title: "INVESTOR RELATION",
+        link: "",
+    },
+    {
+        title: "PRODUCT CATALOGS",
+        link: "",
+    },
+    {
+        title: "MADE BY YONEX",
+        link: "",
+    },
+]
