30601c38c007c42cd9d75773b4d0a11b0ef2163b
SreangLyhour12 <slyhour69@gmail.com>
2024-05-24 20:06:33 +0700

    refactor(homePage): change the padding to not make logo shrink

--- 2nd-mission/assets/js/components/nav-component.js
+++ 2nd-mission/assets/js/components/nav-component.js
@@ -9,12 +9,13 @@ const dataSources = {
     'about-obj': aboutObj
 };
 
-const CreateCard = (cards) => {
+const CreateCard = (cards, gridStyle) => {
+    
     return `
-    <div class="grid grid-cols-4 gap-4 max-w-[1600px] items-center w-[900px] flex-1">
+    <div class="grid ${gridStyle} gap-4 max-w-[1600px] items-center w-[900px] flex-1">
         ${cards.map((card) => {
             return `
-            <div class="flex flex-col items-center justify-center">
+            <div class="flex flex-col items-center justify-center w-[213px]">
                 <a id="hoverLink" href="${card.link}"><img src="${card.img}" alt=""></a>
                 <p class="font-semibold text-[15px] text-black">${card.title}</p>
             </div>
@@ -26,9 +27,10 @@ const CreateCard = (cards) => {
 
 class SlideShow extends HTMLElement {
     connectedCallback() {
+        const gridStyle = this.getAttribute('grid-style')
         const dataSource = this.getAttribute('data-source');
         const data = dataSources[dataSource] || [];
-        this.innerHTML = CreateCard(data);
+        this.innerHTML = CreateCard(data, gridStyle);
     }
 }
 
