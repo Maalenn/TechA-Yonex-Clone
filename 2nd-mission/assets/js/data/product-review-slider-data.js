e21032ed1ecf0146c88091ec8d5316804f2d07fa
Khun Malen <malen8622@gmail.com>
2024-05-25 14:57:12 +0700

    refactor(productReview): change silder in product review page into component

---
+++ 2nd-mission/assets/js/data/product-review-slider-data.js
@@ -0,0 +1,194 @@
+export const sliderBadminton = [
+    {
+        imgSlide: "../assets/images/product-review/al1.webp",
+        name: "viktor axelsen",
+        nation: "denmark"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al2.webp",
+        name: "An se young",
+        nation: "korea"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al3.webp",
+        name: "anane yamaguchi",
+        nation: "japan"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al4.webp",
+        name: "chen yu fei",
+        nation: "china"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al5.webp",
+        name: "kunlavut vididsarn",
+        nation: "thailand"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al6.webp",
+        name: "carolina marin",
+        nation: "spain"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al7.jpeg",
+        name: "Ratchanok Intanon",
+        nation: "thailand"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al8.webp",
+        name: "Chou Tien Chen",
+        nation: "Chinese Taipei"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al9.webp",
+        name: "Lucas Mazur",
+        nation: "France"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al10.jpeg",
+        name: "Chirag Shetty",
+        nation: "India"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al11.webp",
+        name: "Chen Qing Chen",
+        nation: "China"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al12.webp",
+        name: "Jia Yi fan",
+        nation: "China"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al13.webp",
+        name: "lee so hee",
+        nation: "korea"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al14.webp",
+        name: "aaron chia",
+        nation: "malaysia"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al15.webp",
+        name: "soh wooi tik",
+        nation: "malaysia"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al16.webp",
+        name: "kim so yeong",
+        nation: "korea"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al17.webp",
+        name: "kong hee yong",
+        nation: "korea"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al18.webp",
+        name: "fajar alfian",
+        nation: "indonesia"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al19.webp",
+        name: "zheng di wei",
+        nation: "china"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al20.webp",
+        name: "huang ya qiong",
+        nation: "china"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al21.webp",
+        name: "arisa higashino",
+        nation: "japan"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al22.webp",
+        name: "dechaopl puavaranukroh",
+        nation: "thailand"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al23.webp",
+        name: "sapsiree taerattanachai",
+        nation: "thailand"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al38.webp",
+        name: "delphine delrue",
+        nation: "france"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al24.webp",
+        name: "peter gade",
+        nation: "denmark"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al25.webp",
+        name: "taufik hidayat",
+        nation: "indonesia"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al26.webp",
+        name: "lee yang",
+        nation: "chinese taipei"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al27.webp",
+        name: "marcus fernaldi gideon",
+        nation: "indonesia"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al28.webp",
+        name: "kevin sanjaya",
+        nation: "indonesia"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al29.webp",
+        name: "kento momota",
+        nation: "japan"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al30.webp",
+        name: "takuro hoki",
+        nation: "japan"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al31.webp",
+        name: "huang dong ping",
+        nation: "china"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al32.webp",
+        name: "wang yi lyu",
+        nation: "china"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al33.webp",
+        name: "michelle li",
+        nation: "canda"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al34.webp",
+        name: "lakshya sen",
+        nation: "india"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al35.webp",
+        name: "nami matsuyama",
+        nation: "japan"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al36.webp",
+        name: "chiharu shida",
+        nation: "japan"
+    },
+    {
+        imgSlide: "../assets/images/product-review/al37.webp",
+        name: "wang chi lin",
+        nation: "chinese taipei"
+    }
+    
+
+]
