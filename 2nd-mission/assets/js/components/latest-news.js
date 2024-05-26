3a86a64750430cbf001ce70dcc63030a57ba76ae
Prak Sreypov <sreypovprak88@gmail.com>
2024-05-26 12:09:52 +0700

    refactor(newsPage): change font size and padding of news contents

--- 2nd-mission/assets/js/components/latest-news.js
+++ 2nd-mission/assets/js/components/latest-news.js
@@ -10,20 +10,24 @@ const LatestNewsContainer = (newsContents) => {
             <div class="top-block mx-[16px] grid-cols-10 gap-[10px] md:mr-[16px] md:grid block mt-[10px]">
                 <!-- start left side  -->
                 <div class="col-span-6">
-                    <div class="h-[100%] col-span-7 bg-black" style="grid-area: main;">
+                    <div class="h-[100%] col-span-7 bg-black">
                         <div class="h-[100%] flex flex-col pb-[10px] bg-[#FFFFFF]">
-                        <div class="h-[100%] p-0 pb-[10px]">
-                            <img src="${content.latestNews_Img_left}" alt="image"
-                            class="block object-cover h-[100%] w-[100%] max-w-[100%]">
-                        </div>
-                        <p class="p-[2px] pl-[10px] w-full text[16px] text-[#595959] font-[400] leading-[23px] tracking-[2px]">${content.latestNews_Date_left}</p>
-                        <h3 class="m-[6px] pl-[4px] w-full text-[20px] font-[700] leading-[26px] mb-[10px] text-[#1f2427]">${content.latestNews_Des_left}</h3>
-                        <a href="https://www.yonex.com/news/general/yonex-announces-environmental-vision-2050-to-carry-sport-into-the-future/"
-                            class="pr-[21px] w-full text-[#006cb7]"
-                            aria-label="Yonex announces “Environmental Vision 2050”:  To carry sport into the future">
-                            <span class="pl-[10px] text-[16px] font-[700] leading-[3px]">Read Full Story ></span>
-                            <span class="box-border"></span>
-                        </a>
+                            <div class="h-[100%] p-0 pb-[10px]">
+                                <img src="${content.latestNews_Img_left}" alt="image"
+                                class="block object-cover h-[100%] w-[100%] max-w-[100%]">
+                            </div>
+                            
+                            <div class="px-8">
+                                <p class="w-full text-[12px] text-[#595959] font-[400] leading-[23px]">${content.latestNews_Date_left}</p>
+                                <h3 class="w-full text-[18px] font-[700] leading-[26px] text-[#1f2427]">${content.latestNews_Des_left}</h3>
+                                <a href="https://www.yonex.com/news/general/yonex-announces-environmental-vision-2050-to-carry-sport-into-the-future/"
+                                    class="pr-[21px] w-full text-[#006cb7]"
+                                    aria-label="Yonex announces “Environmental Vision 2050”:  To carry sport into the future">
+                                    <span class="text-[14px] font-[700]">Read Full Story ></span>
+                                    <span class="box-border"></span>
+                                </a>
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
@@ -32,27 +36,30 @@ const LatestNewsContainer = (newsContents) => {
                 <!-- start right side  -->
                 <div class="col-span-4 md:grid">
                     ${content.latestNews_content_right.map((item) => `
-                        <div class="md:grid">
-                            <div class="flex flex-col bg-[#fff] pb-[10px]">
+                        <div class="md:grid bg-[#fff]">
+                            <div class="flex flex-col pb-[10px]">
                             <div class="pb-[10px]">
                                 <img src="${item.right_img}" alt="image" class="top-blocks__item__image">
                             </div>
 
-                            <p class="p-[2px] pl-[10px] w-full text-[16px] text-[#595959] font-[400] leading-[23px] tracking-[2px]">
-                                ${item.right_date}
-                            </p>
+                            <div class="px-8">
+                                <p class="w-full text-[12px] text-[#595959] font-[400] leading-[23px]">
+                                    ${item.right_date}
+                                </p>
 
-                            <h3 class="m-[6px] pl-[4px] w-full text-[20px] font-[700] leading-[26px] mb-[10px] text-[#1f2427]">
-                                ${item.right_des}
-                            </h3>
+                                <h3 class="w-full text-[18px] font-[700] leading-[26px] text-[#1f2427]">
+                                    ${item.right_des}
+                                </h3>
+
+                                <a href="" class="w-full text-[#006cb7] mt-10">
+                                    <span class="text-[14px] font-[700]">Read Full Story ></span>
+                                </a>
                             </div>
 
-                            <a href="https://www.yonex.com/news/badminton/upgrade-your-comfort-and-fit-experience-with-the-new-power-cushion-88-dial/"
-                            class="pr-[21px] w-full text-[#006cb7]"
-                            aria-label="Upgrade Your Comfort and Fit Experience with the New POWER CUSHION 88 DIAL">
-                            <span class="pl-[10px] text-[16px] font-[700] leading-[3px]">Read Full Story ></span>
-                            <span class="top-blocks__item__link__gt"></span>
-                            </a>
+                            
+                            </div>
+
+                            
                         </div>
                     `).join("")}
                 </div>
