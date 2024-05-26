37adf2b01b3d77608eeceed8fe1163f590e165ba
Prak Sreypov <sreypovprak88@gmail.com>
2024-05-26 11:48:34 +0700

    refactor(newsPage): integrate data of all latest news in component

--- 2nd-mission/assets/js/data/all-latest-news-data.js
+++ 2nd-mission/assets/js/data/all-latest-news-data.js
@@ -3,23 +3,17 @@ export const AllLatestNewsData = [
         latestNews_Img_left: "../assets/images/news-img/1.1.webp",
         latestNews_Date_left: "April 15, 2024",
         latestNews_Des_left: "Yonex announces “Environmental Vision 2050”: To carry sport into the future",
-        // latestNews_Img_right: [
-        //     "../assets/images/news-img/1.2.webp",
-        //     "https://www.yonex.com/media/wysiwyg/news/tennis/news_240517_960_2.jpg"
-        // ],
-        // latesNews_Date_right: [
-        //     ""
-        // ],
-
-        latestNews_content_right: [
+        latestNews_content_right:[
             {
-                right_img: [
-                    "../assets/images/news-img/1.2.webp",
-                    "https://www.yonex.com/media/wysiwyg/news/tennis/news_240517_960_2.jpg"
-                ]
-            }
-        ]
-
-
-    }
-]
+                right_img: "../assets/images/news-img/1.2.webp",
+                right_date: "May 20, 2024",
+                right_des: "Hyo Joo Kim Claims Her First Victory on the LET at the Aramco Team Series - Korea",
+            },
+            {
+                right_img: "../assets/images/news-img/1.2.webp",
+                right_date: "May 16, 2024",
+                right_des: "Roland-Garros 2024: Keeping it fresh on the clay with Yonex’s Paris Collection",
+            },
+        ],
+    },
+];
