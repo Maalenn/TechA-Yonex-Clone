23612485bf4869621542750b86e52114eb97e058
chounan <tch23125@gmail.com>
2024-05-26 16:05:37 +0700

    refactor(athletProfile): fix carousel function

--- 2nd-mission/assets/js/components/athlete-equipment.js
+++ 2nd-mission/assets/js/components/athlete-equipment.js
@@ -1,10 +1,12 @@
-import {BadmintonAthleteDataEquipment} from '../data/athlete-equip-data.js'
+import { BadmintonAthleteDataEquipment } from '../data/athlete-equip-data.js'
 
 const equipDataSource = {
     'equipImg' :  BadmintonAthleteDataEquipment
 }
 
 const EquipmentContainer = (listEquipments) => {
+    
+
     return `
     <div class="slideShow no-scrollbar grid grid-cols-[repeat(2,400px)] min-[345px]:grid-cols-[repeat(2,500px)] min-[475px]:grid-cols-[repeat(2,700px)]  overflow-auto md:flex md:justify-between my-0 mx-auto max-w-[1160px] ">
         ${listEquipments.map((equipli) =>  `
@@ -14,43 +16,38 @@ const EquipmentContainer = (listEquipments) => {
                 ${equipli.productname}
             </span>
         </div>
-        
         `).join("")}
         
-        <div class="back absolute top-[47%] right-[30px] bg-[#f3f2f2] p-5 md:hidden"
-            onclick="slideArrow(1)">
+        <div class="back absolute top-[47%] right-[30px] bg-[#f3f2f2] p-5 md:hidden" id="nextBtn">
             <img class="w-[1.5rem]" src="../assets/images/icon/arrow-right.svg" alt="" />
         </div>
-        <div class="next absolute top-[47%] left-[30px] bg-[#f3f2f2] p-5 md:hidden"
-            onclick="slideArrow(-1)">
-            <img class="w-[1.5rem]" src="../assets/images/icon/arrow-left.svg"" alt="" />
+        <div class="next absolute top-[47%] left-[30px] bg-[#f3f2f2] p-5 md:hidden" id="prevBtn">
+            <img class="w-[1.5rem]" src="../assets/images/icon/arrow-left.svg" alt="" />
         </div>
     </div>
-    `
+    `;
 }
 
 class ListComponent extends HTMLElement {
     connectedCallback(){
+        let curSlides = 0;
+        function slideArrow(n) {
+            const carousel = document.querySelector(".slideShow");
+            const images = carousel.querySelectorAll(".slide-list");
+            const imageWidth = images[0].clientWidth;
+            curSlides = Math.max(0, Math.min(curSlides + n, images.length - 1));
+            carousel.scrollTo({ left: curSlides * imageWidth, behavior: "smooth" });
+        }
         const dataAtt = this.getAttribute("data-source")
         const dataSourceKey = equipDataSource[dataAtt]
-        this.innerHTML = EquipmentContainer(dataSourceKey)
+        this.innerHTML = EquipmentContainer(dataSourceKey);
+        
+        // Add event listeners after the container has been added to the DOM
+        const nextBtn = document.getElementById('nextBtn');
+        const prevBtn = document.getElementById('prevBtn');
+        
+        nextBtn.addEventListener('click', () => slideArrow(1));
+        prevBtn.addEventListener('click', () => slideArrow(-1));
     }
 }
-customElements.define("li-equipment", ListComponent)
-
-
-
-
-
-
-let curSlides = 0;
-function slideArrow(n) {
-  const carousel = document.querySelector(" .slideShow ");
-  const images = carousel.querySelectorAll(".slide-list");
-  const imageWidth = images[0].clientWidth;
-  curSlides = Math.max(0, Math.min(curSlides + n, images.length - 1));
-  carousel.scrollTo({ left: curSlides * imageWidth, behavior: "smooth" });
-}
-
-
-
+customElements.define("li-equipment", ListComponent);
