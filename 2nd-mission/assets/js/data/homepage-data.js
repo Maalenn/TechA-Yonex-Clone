7e03ac2e5b9e8d4c9d4ea83e0a28e3b6ee77b33b
SreangLyhour12 <slyhour69@gmail.com>
2024-05-26 13:41:28 +0700

    refactor(homePage): integrate data of hero banner with component

---
+++ 2nd-mission/assets/js/data/homepage-data.js
@@ -0,0 +1,17 @@
+export const heroBanner = [
+    {
+        imgBanner: [
+            '../assets/images/homePage-hero/Lucas.webp',
+            '../assets/images/homePage-hero/badminton-cover.webp',
+            '../assets/images/homePage-hero/badminton-image.webp',
+            '../assets/images/homePage-hero/beyond-i-see.webp',
+            '../assets/images/homePage-hero/bwis_manifest_1002.webp',
+            '../assets/images/homePage-hero/counter-drive.webp',
+            '../assets/images/homePage-hero/ezong.webp',
+            '../assets/images/homePage-hero/fly-direction.webp',
+            '../assets/images/homePage-hero/made-by-yonex-desktop.webp',
+            '../assets/images/homePage-hero/new-dawn.webp',
+            '../assets/images/homePage-hero/shoes.webp',
+        ],
+    }
+];
