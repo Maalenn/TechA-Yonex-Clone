7a61ddf49adcbf3a953f828e73bd1355cd32aedd
SreangLyhour12 <slyhour69@gmail.com>
2024-05-27 19:27:06 +0700

    refactor(homePage): integrate data of title button into component

--- 2nd-mission/assets/js/data/homepage-data.js
+++ 2nd-mission/assets/js/data/homepage-data.js
@@ -1,20 +1,46 @@
 export const heroBanner = [
     {
-        imgBanner: [
-            '../assets/images/homePage-hero/Lucas.webp',
-            '../assets/images/homePage-hero/badminton-cover.webp',
-            '../assets/images/homePage-hero/badminton-image.webp',
-            '../assets/images/homePage-hero/beyond-i-see.webp',
-            '../assets/images/homePage-hero/bwis_manifest_1002.webp',
-            '../assets/images/homePage-hero/counter-drive.webp',
-            '../assets/images/homePage-hero/ezong.webp',
-            '../assets/images/homePage-hero/fly-direction.webp',
-            '../assets/images/homePage-hero/made-by-yonex-desktop.webp',
-            '../assets/images/homePage-hero/new-dawn.webp',
-            '../assets/images/homePage-hero/shoes.webp',
-        ],
-    }
-];
+        img:'../assets/images/homePage-hero/Lucas.webp',
+        titleB1: 'Watch Video',
+    },
+    {
+        img:'../assets/images/homePage-hero/badminton-cover.webp',
+        titleB1: 'Learn More',
+    },
+    {
+        img:'../assets/images/homePage-hero/badminton-image.webp',
+        titleB1: 'Read More',
+    },
+    {
+        img:'../assets/images/homePage-hero/beyond-i-see.webp',
+        titleB1: 'Watch Video',
+    },
+    {
+        img:'../assets/images/homePage-hero/bwis_manifest_1002.webp',
+        titleB1: 'Read More',
+    },
+    {
+        img:'../assets/images/homePage-hero/ezong.webp',
+        titleB1: 'Learn More',
+    },
+    {
+        img:'../assets/images/homePage-hero/fly-direction.webp',
+        titleB1: 'Read More',
+    },
+    {
+        img:'../assets/images/homePage-hero/made-by-yonex-desktop.webp',
+        titleB1: 'Learn More',
+    },
+    {
+        img:'../assets/images/homePage-hero/new-dawn.webp',
+        titleB1: 'Learn More',
+    },
+    {
+        img:'../assets/images/homePage-hero/shoes.webp',
+        titleB1: 'Read More',
+    },
+
+]
 
 export const heroRepsonsive = [
     {
