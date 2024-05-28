8375e4917074a8354e17ecc48f62bbc4aa661cc4
Rotha SAMON <samonrotha@gmail.com>
2024-05-28 14:20:43 +0700

    refactor(newsPage): Linking the stpry pages to the cards

--- 2nd-mission/assets/js/data/all-latest-news-data.js
+++ 2nd-mission/assets/js/data/all-latest-news-data.js
@@ -3,7 +3,7 @@ export const AllLatestNewsData = [
         latestNews_Img_left: "https://www.yonex.com/media/wysiwyg/news/tennis/news240522_960_4.jpg",
         latestNews_Date_left: "May 22, 2024",
         latestNews_Des_left: "Roland-Garros 2024: Ruud and Rybakina lead Team Yonex in Paris",
-        latestNews_Link_left: "",
+        latestNews_Link_left: "../public/roland-garros-story.html",
         latestNews_content_right:[
             {
                 right_img: "https://www.yonex.com/media/wysiwyg/news/golf/news240520_960_2.jpg",
@@ -15,7 +15,7 @@ export const AllLatestNewsData = [
                 right_img: "https://www.yonex.com/media/wysiwyg/news/tennis/news_240517_960_5.jpg",
                 right_date: "May 16, 2024",
                 right_des: "Roland-Garros 2024: Keeping it fresh on the clay with Yonex’s Paris Collection",
-                right_link: "",
+                right_link: "../public/roland-garros-story.html",
             },
         ],
     },
