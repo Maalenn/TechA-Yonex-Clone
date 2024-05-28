dcb7d7b0345d396b123c501b5ced5bf43955c442
Prak Sreypov <sreypovprak88@gmail.com>
2024-05-28 17:51:34 +0700

    refactor(homePage): change styles of side bar when responsive

--- 2nd-mission/assets/js/components/product-review-sidebar.js
+++ 2nd-mission/assets/js/components/product-review-sidebar.js
@@ -14,7 +14,7 @@ const SideBarContainer = (sidebarContents) => {
         <form class="">
           <select id="countries" class="text-[16px] p-3 bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-full dark:border-[#1b1a1a] dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500">
             ${sidebarContents.map(item => `
-              <option value="${item}">${item}</option>
+              <option value="${item.title}"><a href="${item.link}">${item.title}</a></option>
             `).join('')}
           </select>
         </form>
